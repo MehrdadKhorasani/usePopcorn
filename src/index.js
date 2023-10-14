121d7a820ba3c7d93ed4788a733edc07a1701e53
MehrdadKhorasani <mehrdadaria9@gmail.com>
2023-10-14 18:17:34 +0330

    showing movie detailes done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./StarRating";
+import "./index.css";
+import App from "./App";
+// import StarRating from "./StarRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} />
+    <App />
+    {/* <StarRating maxRating={5} />
     <StarRating maxRating={10} size={24} color="red" defaultRating={3} />
-    <StarRating messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]} />
+    <StarRating messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]} /> */}
   </React.StrictMode>
 );
